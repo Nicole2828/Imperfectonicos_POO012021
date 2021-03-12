0aeae65785d702087485220c7a5b914c22beb7cf
Nicole2828 <00283320@uca.edu.sv>
2021-03-12 00:10:57 -0600

    Assigned tasks

---
+++ GuideWork/exampleg.cs
@@ -0,0 +1,32 @@
+using System;
+// Primer paso crear el programa del ejemplo guiadado 
+namespace exampleg
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+
+            Console.WriteLine( "Enter a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a,b)} ");
+
+        }
+        
+        // 1)Tasks: Hacer la primera modificación de la funcion AddNumbers
+        // (convertirlo en resta) /
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+            
+        }
+    }
+}
+
+//2) Tasks: Hacer la segunda modificación de la funcion AddNumbers
+// (convertirlo en multiplicación) 
