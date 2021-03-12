dae3965dff04fb8ed681aa268da81ffa2b07b9ba
Nicole2828 <00283320@uca.edu.sv>
2021-03-11 22:23:56 -0600

    Starting the work

---
+++ GuideWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuideWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
